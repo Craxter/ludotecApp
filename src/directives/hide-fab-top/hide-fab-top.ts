8228c13e71d0721065cb5c6e0fbfd9c8a95408ce
Ismael Villaverde <craxter@hotmail.es>
2018-06-08 17:37:07 +0200

    Arreglada directiva fab

--- src/directives/hide-fab-top/hide-fab-top.ts
+++ src/directives/hide-fab-top/hide-fab-top.ts
@@ -9,29 +9,27 @@ import { Content } from "ionic-angular";
 })
 export class HideFabTopDirective {
 
-  private fabRef;
+  private fab;
   private storedScroll: number = 0;
-  private threshold: number = 10;
 
   constructor(public element: ElementRef, public renderer: Renderer) { }
 
   ngOnInit() {
-    this.fabRef = this.element.nativeElement.getElementsByClassName("fab")[0];
-    this.renderer.setElementStyle(this.fabRef, 'webkitTransition', 'transform 500ms,top 500ms');
-    this.renderer.setElementStyle(this.fabRef, 'display', 'none');
-    this.renderer.setElementStyle(this.fabRef, 'webkitTransform', 'scale3d(.1,.1,.1)');
-}
+    this.fab = this.element.nativeElement.getElementsByClassName("fab")[0];
+    this.renderer.setElementStyle(this.fab, 'transition', 'transform 500ms,top 500ms');
+    this.renderer.setElementStyle(this.fab, 'display', 'none');
+  }
 
   handleScroll(event: Content) {
-    if (event.scrollTop - this.storedScroll < this.threshold) {
-      this.renderer.setElementStyle(this.fabRef, 'display', 'block');
-      this.renderer.setElementStyle(this.fabRef, 'top', '0');
-      this.renderer.setElementStyle(this.fabRef, 'webkitTransform', 'scale3d(1,1,1)');
-    } else if (event.scrollTop - this.storedScroll > 0 && event.scrollTop < 100) {
-      this.renderer.setElementStyle(this.fabRef, 'top', '60px');
-      this.renderer.setElementStyle(this.fabRef, 'webkitTransform', 'scale3d(.1,.1,.1)');
+    if (event.scrollTop - this.storedScroll > 0 || event.scrollTop < 100) {
+      this.renderer.setElementStyle(this.fab, 'top', '60px');
+      this.renderer.setElementStyle(this.fab, 'transform', 'scale3d(.1,.1,.1)');
+    } else if (event.scrollTop - this.storedScroll < 0) {
+      this.renderer.setElementStyle(this.fab, 'display', 'block');
+      this.renderer.setElementStyle(this.fab, 'top', '0');
+      this.renderer.setElementStyle(this.fab, 'transform', 'scale3d(1,1,1)');
     }
-    // console.log(event.scrollTop - this.storedScroll);
+    console.log(event.scrollTop);
     this.storedScroll = event.scrollTop;
   }
 }
